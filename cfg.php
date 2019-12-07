1f611415de7c2aec534ec2f5519a627a9e14b019
badwordking <51959848+badwordking@users.noreply.github.com>
2019-12-07 18:11:12 +0700

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -1,8 +1,8 @@
 <?php
 
 //MASUKAN DATA MILIK KALIAN!!!
-$deviceid = "xxxxxxx";
-$token = "xxxxxx";
-$appid = "xxxxxxxxx";
-$installid = "xxxxxxx";
-$clientkey = "xxxxxxx";
+$deviceid = "c1cfca29db3422c5";
+$token = "r:82bb680580766b662b4ec1e7c8205f0a";
+$appid = "Kjqo1GEmwkKzSvvKXPDgXlVRPIgejm09qJlcpxPz";
+$installid = "d348f21f-6428-409b-9e2b-4e73321f35a9";
+$clientkey = "rJ1UujqtBXeL33bnlTsggliQGbIpa5uEugDLPOQL";
